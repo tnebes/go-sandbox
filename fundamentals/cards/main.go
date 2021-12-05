9ce02596cb655af5052172f6affbc64190b54ca5
tnebes <tomislav.nebes@ecx.io>
2021-12-05 16:56:45 +0100

    Types, functions and receivers

--- fundamentals/cards/main.go
+++ fundamentals/cards/main.go
@@ -1,21 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	var cards = []string{newCard(), newCard()}
-
-	fmt.Println(cards)
-	// one way of doing it
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-	// alternative way of doing it
-	for i := 0 ; i < len(cards) ; i++ {
-		fmt.Println(cards[i])
-	}
-
+	cards := deck{newCard(), newCard(), "Bluh of derp"}
+	cards.print()
 }
 
 func newCard() string {
